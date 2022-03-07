6d090a53d711e7012f90fab1db32a87b8a5edce7
Kiryl <katana96@mail.ru>
2022-03-07 15:55:30 +0300

    Finish Exercise 3.12

--- 3.12.cpp
+++ 3.12.cpp
@@ -6,42 +6,46 @@ class Account
 {
     public:
 
-    Account(int balanc)
+    Account(int balanc) // Определяем конструктор
     {
         setBalance( balanc );
     }
 
-    void setBalance( int balanc )
+    void setBalance( int balanc ) // Функция для установки баланса
     {
         if (balanc>0)
         {
             accountBalanc=balanc;
         } else
-        accountBalanc=0;
+            accountBalanc=0;
     }
 
 
-    int getBalance()
+    int getBalance() // Функция для получения баланса
     {   
         
         return accountBalanc;
         
     }
 
-    int credit(int credit)
+    int credit(int credit) // Функция добовляет деньги на счёт
     {
         accountBalanc=accountBalanc + credit;
         return accountBalanc;
     }
 
-    int debit(int debit)
+    int debit(int debit) // Функция снимает деньги со счёта
     {   
         if(debit<=accountBalanc)
         {
             accountBalanc=accountBalanc - debit;
             return accountBalanc;
-        }
-        return accountBalanc;
+        } else 
+        {
+            cout << "Error!!!The debit increases the balance!!!" << endl;
+            return accountBalanc;
+        };
+        
     }
 
     private:
@@ -52,15 +56,35 @@ class Account
 
 int main()
 {
-    Account Balanc1 (1000);
-    Account Balanc2 (2200);
+    int balanc1;
+    int debit1;
+    int credit1;
 
-    cout << Balanc1. getBalance() << endl;
-    cout << Balanc2. getBalance();
+        cout << "Enter current balance 1: ";
+        cin >> balanc1;
 
-    cout << Balanc1. debit(15000) << endl;
-    cout << Balanc2. debit(1500);
+        Account Balanc1 (balanc1);
 
-    
+    cout << "Enter debit: ";
+    cin >> debit1;
+    Balanc1. debit(debit1);
+    cout << "Enter credit: ";
+    cin >> credit1;
+    cout << "Current balance: " << Balanc1. credit(credit1) << endl;
 
+    int balanc2;
+    int debit2;
+    int credit2;
+
+        cout << endl << "Enter current balance 2: ";
+        cin >> balanc2;
+
+        Account Balanc2 (balanc2);
+
+    cout << "Enter debit: ";
+    cin >> debit2;
+    Balanc2. debit(debit2);
+    cout << "Enter credit: ";
+    cin >> credit2;
+    cout << "Current balance: " << Balanc2. credit(credit2);
 }
